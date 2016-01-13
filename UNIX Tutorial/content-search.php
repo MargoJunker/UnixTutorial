73e1c63ffe4b26180203f97cacdfb3a6b127189a
Margo Junker <margo.junker@outlook.com>
2016-01-13 13:50:23 +0200

    New Beginning

---
+++ UNIX Tutorial/content-search.php
@@ -0,0 +1,15 @@
+<?php?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+    <div class="col-sm-12 tutorial-item-row" onclick="location.href='<?php the_permalink(); ?>';" style="cursor: pointer;">
+                <div class="col-sm-4 tutorial-item-img">
+                    <div class="tutorial-img img-responsive hidden-xs"><?php if ( has_post_thumbnail() ) {
+	the_post_thumbnail();
+ } ?></div>
+                </div>
+                <div class="col-sm-8 tutorial-item-description">
+                    <h2 class="tutorial-item-h2"><?php the_title(); ?></h2>
+                    <p class="tutorial-item-p"><?php the_excerpt();?></p>
+                </div>
+            </div>
+</article><!-- #post-## -->
---
+++ UNIX Tutorial/content-single.php
@@ -0,0 +1,23 @@
+<?php?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<?php the_title( '<h1 class="entry-title tutorial-header">', '</h1>' ); ?>
+		<div class="entry-meta">
+		</div><!-- .entry-meta -->
+	</header><!-- .entry-header -->
+
+	<div class="entry-content">
+		<?php the_content(); ?>
+		<?php
+			wp_link_pages( array(
+				'before' => '<div class="page-links">' . __( 'Pages:', 'underscores_theme' ),
+				'after'  => '</div>',
+			) );
+		?>
+	</div><!-- .entry-content -->
+
+	<footer class="entry-footer">
+		<?php underscores_theme_entry_footer(); ?>
+	</footer><!-- .entry-footer -->
+</article><!-- #post-## -->
